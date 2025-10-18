a1d621db977be37b2d6fc8c4f90833a56782c953
Daniel Stokes <222254080+stokes0224@users.noreply.github.com>
2025-10-18 18:34:41 +0100

    FEAT: Full Cipher and decoder.

--- README.md
+++
@@ -1,1 +0,0 @@
-# caeserCipher
---
+++ decoder.rb
@@ -0,0 +1,33 @@
+puts "What is your coded message?"
+string = gets.chomp.chars
+puts "What was your key?"
+shift = gets.chomp.to_i
+
+upper_case = ('A'..'Z') # (char - 65 - shift) % 26 + 65
+lower_case = ('a'..'z') # (char - 97 - shift) % 26 + 97
+
+
+def decoder(string, shift)
+  result = []
+  string.each do |char|
+    if ('A'..'Z').include?(char)
+      ascii_upper = char.ord
+      shifted_upper = (ascii_upper - 65 - shift)%26+65
+      new_char = shifted_upper.chr
+      result << new_char
+    elsif ('a'..'z').include?(char)
+      ascii_lower = char.ord
+      shifted_lower = (ascii_lower - 97 - shift)%26+97
+      new_char = shifted_lower.chr
+      result << new_char
+    else
+      result << char
+    end
+  end
+  result.join
+end
+
+
+
+
+puts decoder(string, shift)
